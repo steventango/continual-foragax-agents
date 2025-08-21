1469fe00c090886e4db139069c5cd431a8a64dce
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-08-21 14:02:07 -0600

    Add Foragax

---
+++ src/environments/Foragax.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+import jax
+from foragax.registry import make
+from rlglue import BaseEnvironment
+
+
+class Foragax(BaseEnvironment):
+    def __init__(self, seed: int, **env_params):
+        self.env = make(**env_params)
+        self.seed = seed
+        self.state = None
+        self.key = jax.random.PRNGKey(seed)
+
+    def start(self) -> Any:
+        self.key, env_reset_key = jax.random.split(self.key)
+        obs, self.state = self.env.reset(env_reset_key)
+        return obs
+
+    def step(self, action: int):
+        self.key, env_step_key = jax.random.split(self.key)
+        obs, self.state, reward, done, info = self.env.step(env_step_key, self.state, action)
+        return obs, reward, done, done, info
---
+++ src/problems/Foragax.py
@@ -0,0 +1,15 @@
+from ml_instrumentation.Collector import Collector
+from environments.Foragax import Foragax as Env
+from experiment.ExperimentModel import ExperimentModel
+from problems.BaseProblem import BaseProblem
+
+class Foragax(BaseProblem):
+    def __init__(self, exp: ExperimentModel, idx: int, collector: Collector):
+        super().__init__(exp, idx, collector)
+
+        env = Env(self.seed, **self.env_params)
+        self.env = env
+        self.actions = env.env.action_space(env.env.default_params).n
+
+        self.observations = env.env.observation_space(env.env.default_params).shape
+        self.gamma = 0.99
