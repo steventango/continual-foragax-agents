7ea12a0798ceb4159b8797f810c6736d33f567d4
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-08-22 23:11:55 -0600

    JIT Foragax.step

--- src/environments/Foragax.py
+++ src/environments/Foragax.py
@@ -1,6 +1,7 @@
 from typing import Any
-
+from functools import partial
 import jax
+from foragax.env import EnvState
 from foragax.registry import make
 from rlglue import BaseEnvironment
 
@@ -18,8 +19,13 @@ class Foragax(BaseEnvironment):
         return obs
 
     def step(self, action: int):
-        self.key, env_step_key = jax.random.split(self.key)
-        obs, self.state, reward, done, info = self.env.step(
-            env_step_key, self.state, action
+        (self.state, self.key), (obs, reward, done, done, info) = self._step(
+            self.state, action, self.key
         )
-        return obs, reward, done, done, info
+        return (obs, reward, done, done, info)
+
+    @partial(jax.jit, static_argnums=0)
+    def _step(self, state: EnvState, action: int, key: jax.Array):
+        key, env_step_key = jax.random.split(key)
+        obs, state, reward, done, info = self.env.step(env_step_key, state, action)
+        return (state, key), (obs, reward, done, done, info)
