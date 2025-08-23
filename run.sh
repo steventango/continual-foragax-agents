49fce61c57edc17b8ca58e9317fadca0aed358cf
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-08-23 12:42:05 -0600

    Run 100k steps

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 mkdir -p results/logs/
-for i in {0..0}
+for i in {0..4}
 do
-  # { time python src/continuing_main.py -e experiments/foragax-sweep/ForagaxTwoBiomeSmall/DQN.json -i $i &> results/logs/DQN_$i.log; } 2> results/logs/DQN_$i.time &
+  { time python src/continuing_main.py -e experiments/foragax-sweep/ForagaxTwoBiomeSmall/DQN.json -i $i &> results/logs/DQN_$i.log; } 2> results/logs/DQN_$i.time &
   { time python src/continuing_main.py -e experiments/foragax-sweep/ForagaxTwoBiomeSmall/EQRC.json -i $i &> results/logs/EQRC_$i.log; } 2> results/logs/EQRC_$i.time &
 done
 wait
