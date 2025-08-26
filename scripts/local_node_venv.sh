0a15be66b5909b732ad16b757da24ccc0b00d82a
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-08-26 10:39:57 -0600

    Fix dependencies

--- scripts/local_node_venv.sh
+++ scripts/local_node_venv.sh
@@ -3,16 +3,12 @@
 #SBATCH --time=00:55:00
 #SBATCH --account=rrg-whitem
 
-module load python/3.11 arrow/19 gcc rust
+module load python/3.11 arrow/19 gcc opencv rust swig
 
-cp $path/requirements.txt $SLURM_TMPDIR/
+cp $path/pyproject.toml $SLURM_TMPDIR/
 cd $SLURM_TMPDIR
 python -m venv .venv
 source .venv/bin/activate
-
-# TODO: for some reason, pip cannot install any of the current wheels for this package.
-# this is a pretty bad hack, but...
-pip install --platform manylinux_2_28_x86_64 --no-deps --target .venv/lib/python3.11/site-packages connectorx
 
 pip install .
 
