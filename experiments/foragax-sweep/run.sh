0d07c79c1d33ce352cf9fb682af9a230322b6312
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-08-25 18:41:41 -0600

    Add hypers script to run.sh

--- experiments/foragax-sweep/run.sh
+++ experiments/foragax-sweep/run.sh
@@ -1,3 +1,4 @@
 python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall/DQN.json --entry src/continuing_main.py
 python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall/EQRC.json --entry src/continuing_main.py
+python experiments/foragax-sweep/hypers.py
 python experiments/foragax-sweep/learning_curve.py
