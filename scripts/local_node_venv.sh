f8c3bcd98a68cc63cad19ebb93b38bc420339336
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-08-26 00:22:30 -0600

    Fix install connectorx>=0.4.2

--- scripts/local_node_venv.sh
+++ scripts/local_node_venv.sh
@@ -9,12 +9,12 @@ cp $path/requirements.txt $SLURM_TMPDIR/
 cd $SLURM_TMPDIR
 python -m venv .venv
 source .venv/bin/activate
-pip install .
 
 # TODO: for some reason, pip cannot install any of the current wheels for this package.
 # this is a pretty bad hack, but...
-curl https://files.pythonhosted.org/packages/38/9c/3a3a831bfbd30fdedd61994d35df41fd0d47145693fe706976589214f811/connectorx-0.3.2-cp311-cp311-manylinux_2_28_x86_64.whl --output connectorx-0.3.2-cp311-cp311-linux_x86_64.whl
-pip install connectorx-0.3.2-cp311-cp311-linux_x86_64.whl
+pip install --platform manylinux_2_28_x86_64 --no-deps --target .venv/lib/python3.11/site-packages connectorx
+
+pip install .
 
 tar -cavf venv.tar.xz .venv
 cp venv.tar.xz $path/
