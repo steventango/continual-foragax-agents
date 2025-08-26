84085c63782efa4b4e5ce4a44fe4986caa5dba9d
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-08-26 13:03:41 -0600

    Split configs by aperture size

--- experiments/foragax-sweep/run.sh
+++ experiments/foragax-sweep/run.sh
@@ -1,4 +1,6 @@
-python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall/DQN.json --entry src/continuing_main.py
-python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall/EQRC.json --entry src/continuing_main.py
+python scripts/local.py --runs 1 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-3/DQN.json --entry src/continuing_main.py
+python scripts/local.py --runs 1 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-3/EQRC.json --entry src/continuing_main.py
+python scripts/local.py --runs 1 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-5/DQN.json --entry src/continuing_main.py
+python scripts/local.py --runs 1 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-5/EQRC.json --entry src/continuing_main.py
 python experiments/foragax-sweep/hypers.py
 python experiments/foragax-sweep/learning_curve.py
