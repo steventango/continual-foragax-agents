471049346835b4f36952e64e220b7bf4dd2c7d26
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-08-27 15:24:02 -0600

    Run RandomAgent

--- experiments/foragax/run.sh
+++ experiments/foragax/run.sh
@@ -1,3 +1,4 @@
 python scripts/local.py --runs 30 -e experiments/foragax/ForagaxTwoBiomeSmall/DQN.json --entry src/continuing_main.py
 python scripts/local.py --runs 30 -e experiments/foragax/ForagaxTwoBiomeSmall/EQRC.json --entry src/continuing_main.py
+python scripts/local.py --runs 30 -e experiments/foragax/ForagaxTwoBiomeSmall-15/Random.json --entry src/continuing_main.py
 python experiments/foragax/learning_curve.py
