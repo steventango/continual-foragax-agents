80038424e8554225bbd7f4db592b85ecc3ee35a9
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-08-29 14:43:34 -0600

    Fix multiprocess run

--- experiments/foragax-vmap-test/run-multiprocess.sh
+++ experiments/foragax-vmap-test/run-multiprocess.sh
@@ -1,7 +1,8 @@
+export XLA_PYTHON_CLIENT_MEM_FRACTION=0.16
 mkdir -p results/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN/logs
-{time python src/continuing_main.py --gpu -e experiments/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN.json -i 0;} &> results/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN/logs/0-multiprocess.log &
-{time python src/continuing_main.py --gpu -e experiments/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN.json -i 1;} &> results/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN/logs/1-multiprocess.log &
-{time python src/continuing_main.py --gpu -e experiments/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN.json -i 2;} &> results/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN/logs/2-multiprocess.log &
-{time python src/continuing_main.py --gpu -e experiments/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN.json -i 3;} &> results/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN/logs/3-multiprocess.log &
-{time python src/continuing_main.py --gpu -e experiments/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN.json -i 4;} &> results/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN/logs/4-multiprocess.log &
+{ time python src/continuing_main.py --gpu -e experiments/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN.json -i 0; } &> results/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN/logs/0-multiprocess.log &
+{ time python src/continuing_main.py --gpu -e experiments/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN.json -i 1; } &> results/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN/logs/1-multiprocess.log &
+{ time python src/continuing_main.py --gpu -e experiments/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN.json -i 2; } &> results/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN/logs/2-multiprocess.log &
+{ time python src/continuing_main.py --gpu -e experiments/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN.json -i 3; } &> results/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN/logs/3-multiprocess.log &
+{ time python src/continuing_main.py --gpu -e experiments/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN.json -i 4; } &> results/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN/logs/4-multiprocess.log &
 wait
