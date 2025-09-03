d01362c13cd388942e989942a3e681df07b9c144
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-02 20:38:37 -0600

    Remove test configs

--- experiments/foragax-vmap-test/run-vmap.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p results/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN/logs\
-
-start=0
-end=199
-indices=($(seq $start $end))
-
-{ time python src/continuing_main.py --gpu -e experiments/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN.json -i ${indices[@]}; } &> results/foragax-vmap-test/ForagaxTwoBiomeSmall-15/DQN/logs/vmap.log
