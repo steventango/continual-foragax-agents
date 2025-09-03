96af259cd559f280036b2b410cc22da6d6e3e109
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-02 23:50:31 -0600

    Plot evaluation

--- experiments/foragax/run.sh
+++ experiments/foragax/run.sh
@@ -1,4 +1,2 @@
-python scripts/local.py --runs 30 -e experiments/foragax/ForagaxTwoBiomeSmall/DQN.json --entry src/continuing_main.py
-python scripts/local.py --runs 30 -e experiments/foragax/ForagaxTwoBiomeSmall/EQRC.json --entry src/continuing_main.py
-python scripts/local.py --runs 30 -e experiments/foragax/ForagaxTwoBiomeSmall-15/Random.json --entry src/continuing_main.py
 python experiments/foragax/learning_curve.py
+python experiments/foragax/auc_fov.py
