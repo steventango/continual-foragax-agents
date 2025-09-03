7a70e790719cc66e672d43afd6c4543e5b3bd970
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-02 18:21:10 -0600

    Lower XLA_PYTHON_CLIENT_MEM_FRACTION

--- scripts/slurm.py
+++ scripts/slurm.py
@@ -59,7 +59,7 @@ srun --ntasks=$SLURM_NNODES --ntasks-per-node=1 tar -xf {venv_origin} -C {venv}
 
 export MPLBACKEND=TKAgg
 export OMP_NUM_THREADS=1
-export XLA_PYTHON_CLIENT_MEM_FRACTION=0.99
+export XLA_PYTHON_CLIENT_MEM_FRACTION=0.95
 {parallel}
     """
 
