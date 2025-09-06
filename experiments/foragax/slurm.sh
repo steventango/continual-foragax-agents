6a3b3039d25b05ae2aa04a1bf130460c030c556e
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-05 23:04:31 -0600

    Run 100 ones

--- experiments/foragax/slurm.sh
+++ experiments/foragax/slurm.sh
@@ -1,7 +1,7 @@
-python scripts/slurm.py --cluster clusters/vulcan-foragax.json --runs 30 --entry src/continuing_main.py --force -e experiments/foragax/ForagaxTwoBiomeSmall-3/DQN-2.json
-python scripts/slurm.py --cluster clusters/vulcan-foragax.json --runs 30 --entry src/continuing_main.py --force -e experiments/foragax/ForagaxTwoBiomeSmall-5/DQN-2.json
-python scripts/slurm.py --cluster clusters/vulcan-foragax.json --runs 30 --entry src/continuing_main.py --force -e experiments/foragax/ForagaxTwoBiomeSmall-7/DQN-2.json
-python scripts/slurm.py --cluster clusters/vulcan-foragax.json --runs 30 --entry src/continuing_main.py --force -e experiments/foragax/ForagaxTwoBiomeSmall-9/DQN-2.json
-python scripts/slurm.py --cluster clusters/vulcan-foragax.json --runs 30 --entry src/continuing_main.py --force -e experiments/foragax/ForagaxTwoBiomeSmall-11/DQN-2.json
-python scripts/slurm.py --cluster clusters/vulcan-foragax.json --runs 30 --entry src/continuing_main.py --force -e experiments/foragax/ForagaxTwoBiomeSmall-13/DQN-2.json
-python scripts/slurm.py --cluster clusters/vulcan-foragax.json --runs 30 --entry src/continuing_main.py --force -e experiments/foragax/ForagaxTwoBiomeSmall-15/DQN-2.json
+python scripts/slurm.py --cluster clusters/vulcan-foragax.json --runs 100 --entry src/continuing_main.py --force -e experiments/foragax/ForagaxTwoBiomeSmall-3/DQN-2.json
+python scripts/slurm.py --cluster clusters/vulcan-foragax.json --runs 100 --entry src/continuing_main.py --force -e experiments/foragax/ForagaxTwoBiomeSmall-5/DQN-2.json
+python scripts/slurm.py --cluster clusters/vulcan-foragax.json --runs 100 --entry src/continuing_main.py --force -e experiments/foragax/ForagaxTwoBiomeSmall-7/DQN-2.json
+python scripts/slurm.py --cluster clusters/vulcan-foragax.json --runs 100 --entry src/continuing_main.py --force -e experiments/foragax/ForagaxTwoBiomeSmall-9/DQN-2.json
+python scripts/slurm.py --cluster clusters/vulcan-foragax.json --runs 100 --entry src/continuing_main.py --force -e experiments/foragax/ForagaxTwoBiomeSmall-11/DQN-2.json
+python scripts/slurm.py --cluster clusters/vulcan-foragax.json --runs 100 --entry src/continuing_main.py --force -e experiments/foragax/ForagaxTwoBiomeSmall-13/DQN-2.json
+python scripts/slurm.py --cluster clusters/vulcan-foragax.json --runs 100 --entry src/continuing_main.py --force -e experiments/foragax/ForagaxTwoBiomeSmall-15/DQN-2.json
