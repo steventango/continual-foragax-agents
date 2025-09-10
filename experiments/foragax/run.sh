1ed0341b63d77e224f422b020092aee4f929ef20
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-10 08:52:58 -0600

    Run Search Oracle without vmap
    It is really slow with vmap

--- experiments/foragax/run.sh
+++ experiments/foragax/run.sh
@@ -1,5 +1,5 @@
 python scripts/local.py --runs 30 -e experiments/foragax/ForagaxTwoBiomeSmall-15/Random.json --entry src/continuing_main.py --gpu
 python scripts/local.py --runs 30 -e experiments/foragax/ForagaxTwoBiomeSmall-15/Search-Nearest.json --entry src/continuing_main.py --gpu
-python scripts/local.py --runs 30 -e experiments/foragax/ForagaxTwoBiomeSmall-15/Search-Oracle.json --entry src/continuing_main.py --gpu
+python scripts/local.py --runs 30 -e experiments/foragax/ForagaxTwoBiomeSmall-15/Search-Oracle.json --entry src/continuing_main.py
 python experiments/foragax/learning_curve.py
 python experiments/foragax/auc_fov.py
