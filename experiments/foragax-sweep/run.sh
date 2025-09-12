bcf07ee9cbd2478d3c52b898afc6c152a172bc5b
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-12 12:28:40 -0600

    Remove gpu

--- experiments/foragax-sweep/run.sh
+++ experiments/foragax-sweep/run.sh
@@ -1,5 +1,5 @@
-python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-15/Random.json --entry src/continuing_main.py --gpu
-python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-15/Search-Nearest.json --entry src/continuing_main.py --gpu
+python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-15/Random.json --entry src/continuing_main.py
+python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-15/Search-Nearest.json --entry src/continuing_main.py
 python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-15/Search-Oracle.json --entry src/continuing_main.py
 python experiments/foragax-sweep/hypers.py
 python experiments/foragax-sweep/learning_curve.py
