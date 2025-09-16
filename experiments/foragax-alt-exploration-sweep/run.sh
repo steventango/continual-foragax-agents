65490a94b70349d542dbca930879f64ca91c6d14
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-16 12:56:19 -0600

    Run baselines

--- experiments/foragax-alt-exploration-sweep/run.sh
+++ experiments/foragax-alt-exploration-sweep/run.sh
@@ -1,5 +1,6 @@
-python scripts/local.py --runs 5 -e experiments/foragax-alt-exploration-sweep/ForagaxTwoBiomeSmall-15/Random.json --entry src/continuing_main.py
-python scripts/local.py --runs 5 -e experiments/foragax-alt-exploration-sweep/ForagaxTwoBiomeSmall-15/Search-Nearest.json --entry src/continuing_main.py
-python scripts/local.py --runs 5 -e experiments/foragax-alt-exploration-sweep/ForagaxTwoBiomeSmall-15/Search-Oracle.json --entry src/continuing_main.py
+python scripts/local.py --runs 5 -e experiments/foragax-alt-exploration-sweep/ForagaxTwoBiomeSmall-15/Random.json --entry src/continuing_main.py &
+python scripts/local.py --runs 5 -e experiments/foragax-alt-exploration-sweep/ForagaxTwoBiomeSmall-15/Search-Nearest.json --entry src/continuing_main.py &
+python scripts/local.py --runs 5 -e experiments/foragax-alt-exploration-sweep/ForagaxTwoBiomeSmall-15/Search-Oracle.json --entry src/continuing_main.py &
+wait
 python experiments/foragax-alt-exploration-sweep/hypers.py
 python experiments/foragax-alt-exploration-sweep/learning_curve.py
