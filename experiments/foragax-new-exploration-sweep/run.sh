ed5d564803d1ea571f3bbd0fd28eeac772538b61
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-16 17:24:44 -0600

    Rename old to new

--- experiments/foragax-new-exploration-sweep/run.sh
+++ experiments/foragax-new-exploration-sweep/run.sh
@@ -1,6 +1,6 @@
-python scripts/local.py --runs 5 -e experiments/foragax-old-exploration-sweep/ForagaxTwoBiomeSmall-15/Random.json --entry src/continuing_main.py &
-python scripts/local.py --runs 5 -e experiments/foragax-old-exploration-sweep/ForagaxTwoBiomeSmall-15/Search-Nearest.json --entry src/continuing_main.py &
-python scripts/local.py --runs 5 -e experiments/foragax-old-exploration-sweep/ForagaxTwoBiomeSmall-15/Search-Oracle.json --entry src/continuing_main.py &
+python scripts/local.py --runs 5 -e experiments/foragax-new-exploration-sweep/ForagaxTwoBiomeSmall-15/Random.json --entry src/continuing_main.py &
+python scripts/local.py --runs 5 -e experiments/foragax-new-exploration-sweep/ForagaxTwoBiomeSmall-15/Search-Nearest.json --entry src/continuing_main.py &
+python scripts/local.py --runs 5 -e experiments/foragax-new-exploration-sweep/ForagaxTwoBiomeSmall-15/Search-Oracle.json --entry src/continuing_main.py &
 wait
-python experiments/foragax-old-exploration-sweep/hypers.py
-python experiments/foragax-old-exploration-sweep/learning_curve.py
+python experiments/foragax-new-exploration-sweep/hypers.py
+python experiments/foragax-new-exploration-sweep/learning_curve.py
