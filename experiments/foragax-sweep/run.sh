6ab2abc93813db85643d7183c1f3e9071cbbcfd9
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-17 23:11:19 -0600

    Rename scripts to follow FTBS-v2 convention

--- experiments/foragax-sweep/run.sh
+++ experiments/foragax-sweep/run.sh
@@ -1,6 +1,6 @@
-python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-15/Random.json --entry src/continuing_main.py &
-python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-15/Search-Nearest.json --entry src/continuing_main.py &
-python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-15/Search-Oracle.json --entry src/continuing_main.py &
+python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-15/Random.json --entry src/continuing_main.py &
+python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-15/Search-Nearest.json --entry src/continuing_main.py &
+python scripts/local.py --runs 5 -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-15/Search-Oracle.json --entry src/continuing_main.py &
 wait
 python experiments/foragax-sweep/hypers.py
 python experiments/foragax-sweep/learning_curve.py
