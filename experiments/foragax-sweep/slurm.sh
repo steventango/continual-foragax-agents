676af980d5c47338741696d3056f3a42ef2cb7b0
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-20 14:39:35 -0600

    Add small buffer variant of L2 Init

--- experiments/foragax-sweep/slurm.sh
+++ experiments/foragax-sweep/slurm.sh
@@ -21,6 +21,15 @@ python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --e
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-45m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-11/DQN_L2_Init.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-45m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-13/DQN_L2_Init.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-15/DQN_L2_Init.json
+
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-15m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-3/DQN_L2_Init_small_buffer.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-5/DQN_L2_Init_small_buffer.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-7/DQN_L2_Init_small_buffer.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-9/DQN_L2_Init_small_buffer.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-45m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-11/DQN_L2_Init_small_buffer.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-45m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-13/DQN_L2_Init_small_buffer.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-15/DQN_L2_Init_small_buffer.json
+
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-15m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-3/DQN_LN.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-5/DQN_LN.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxTwoBiomeSmall-v2-7/DQN_LN.json
