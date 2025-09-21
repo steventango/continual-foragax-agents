de5ff49e3068b20647d66816830081ee2c0bb7a9
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-21 17:49:13 -0600

    Update configs for sweep

--- experiments/foragax-sweep/ForagaxWeather-v1/slurm.sh
+++ experiments/foragax-sweep/ForagaxWeather-v1/slurm.sh
@@ -6,14 +6,6 @@ python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-45m.json --runs 5 --e
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-45m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-13/DQN.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-15/DQN.json
 
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-15m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-3/DQN_small_buffer.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-5/DQN_small_buffer.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-7/DQN_small_buffer.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-9/DQN_small_buffer.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-45m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-11/DQN_small_buffer.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-45m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-13/DQN_small_buffer.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-15/DQN_small_buffer.json
-
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-15m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-3/DQN_L2_Init.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-5/DQN_L2_Init.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-7/DQN_L2_Init.json
@@ -21,14 +13,6 @@ python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --e
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-45m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-11/DQN_L2_Init.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-45m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-13/DQN_L2_Init.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-15/DQN_L2_Init.json
-
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-15m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-3/DQN_L2_Init_small_buffer.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-5/DQN_L2_Init_small_buffer.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-7/DQN_L2_Init_small_buffer.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-9/DQN_L2_Init_small_buffer.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-45m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-11/DQN_L2_Init_small_buffer.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-45m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-13/DQN_L2_Init_small_buffer.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-15/DQN_L2_Init_small_buffer.json
 
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-15m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-3/DQN_LN.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-30m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-5/DQN_LN.json
@@ -41,4 +25,3 @@ python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --en
 python scripts/slurm.py --cluster clusters/vulcan-cpu-15m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-15/Random.json
 python scripts/slurm.py --cluster clusters/vulcan-cpu-15m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-15/Search-Nearest.json
 python scripts/slurm.py --cluster clusters/vulcan-cpu-15m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-15/Search-Oracle.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-15m.json --runs 5 --entry src/continuing_main.py --force -e experiments/foragax-sweep/ForagaxWeather-v1/ForagaxWeather-v1-15/Search-Oyster.json
