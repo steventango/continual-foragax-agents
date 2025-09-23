ff7a27b27c8dbb09526c77f9c761efe6b0770eea
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-23 12:03:14 -0600

    Fix links again

--- experiments/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/run.sh
+++ experiments/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/run.sh
@@ -1,3 +1,3 @@
 mkdir -p results/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/Baselines
-ln -s ../../../result-search-vs-limited-fov-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Oracle.json results/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Oracle.json
-ln -s ../../../result-search-vs-limited-fov-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Brown-Avoid-Green.json results/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Brown-Avoid-Green.json
+ln -s ../../../../result-search-vs-limited-fov-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Oracle results/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Oracle
+ln -s ../../../../result-search-vs-limited-fov-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Brown-Avoid-Green results/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Brown-Avoid-Green
