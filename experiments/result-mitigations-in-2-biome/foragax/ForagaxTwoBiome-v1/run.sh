12ca19cd4c5ed5694c95546e2845491329d038e5
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-23 11:58:37 -0600

    Fix symlinks

--- experiments/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/run.sh
+++ experiments/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/run.sh
@@ -1,1 +1,3 @@
-ln -s results/result-search-vs-limited-fov-2-biome/foragax/ForagaxTwoBiome-v1/Baselines results/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1
+mkdir -p results/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/Baselines
+ln -s ../../../result-search-vs-limited-fov-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Oracle.json results/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Oracle.json
+ln -s ../../../result-search-vs-limited-fov-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Brown-Avoid-Green.json results/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Brown-Avoid-Green.json
