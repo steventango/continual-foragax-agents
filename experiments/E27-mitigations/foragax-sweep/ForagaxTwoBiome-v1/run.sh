e0392b76b73752ecd72b7c468343aa641d6d4f90
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-24 16:34:29 -0600

    Switch to ForagaxTwoBiome-v2

--- experiments/E27-mitigations/foragax-sweep/ForagaxTwoBiome-v1/run.sh
+++
@@ -1,2 +0,0 @@
-python experiments/result-mitigations-in-2-biome/foragax-sweep/ForagaxTwoBiome-v1/hypers.py
-python experiments/result-mitigations-in-2-biome/foragax-sweep/ForagaxTwoBiome-v1/learning_curve.py
--- experiments/E27-mitigations/foragax/ForagaxTwoBiome-v1/run.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p results/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/Baselines
-ln -s ../../../../result-search-vs-limited-fov-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Oracle results/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Oracle
-ln -s ../../../../result-search-vs-limited-fov-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Brown-Avoid-Green results/result-mitigations-in-2-biome/foragax/ForagaxTwoBiome-v1/Baselines/Search-Brown-Avoid-Green
