8da212c458b49dbc8bca14db70233103c15f5224
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-24 16:20:24 -0600

    Update job name and script paths in hypers_job.sh for consistency

--- experiments/E26-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v2/hypers_job.sh
+++ experiments/E26-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v2/hypers_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=aip-amw8
-#SBATCH --job-name=hypers_result-search-vs-limited-fov-2-biome_foragax-sweep_ForagaxTwoBiome-v2
+#SBATCH --job-name=hypers_E26-search-limited-fov_foragax-sweep_ForagaxTwoBiome-v2
 #SBATCH --mem-per-cpu=128G
 #SBATCH --ntasks=1
 #SBATCH --output=../slurm-%j.out
@@ -19,4 +19,6 @@ export XLA_FLAGS="--xla_cpu_multi_thread_eigen=false intra_op_parallelism_thread
 export XLA_PYTHON_CLIENT_MEM_FRACTION=0.95
 export JAX_PLATFORMS=cpu
 
-$SLURM_TMPDIR/.venv/bin/python experiments/result-search-vs-limited-fov-2-biome/foragax-sweep/ForagaxTwoBiome-v2/hypers.py
+$SLURM_TMPDIR/.venv/bin/python experiments/E26-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v2/hypers.py
+
+$SLURM_TMPDIR/.venv/bin/python experiments/E26-search-limited-fov/foragax/ForagaxTwoBiome-v2/generate_frozen_config.py
