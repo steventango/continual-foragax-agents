814163820c3acc28be0269eb008f62fc046a1258
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-24 17:40:41 -0600

    Update slurm.sh to use DQN configurations for biome 15

--- experiments/E28-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v2/slurm.sh
+++ experiments/E28-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v2/slurm.sh
@@ -1,7 +1,7 @@
-python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E28-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v2/9/DQN_B1000.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E28-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v2/9/DQN_B10000.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E28-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v2/9/DQN_B100000.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E28-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v2/15/DQN_B1000.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E28-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v2/15/DQN_B10000.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E28-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v2/15/DQN_B100000.json
 
-python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E28-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v2/9/DQN_world_B1000.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E28-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v2/9/DQN_world_B10000.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E28-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v2/9/DQN_world_B100000.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E28-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v2/15/DQN_world_B1000.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E28-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v2/15/DQN_world_B10000.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E28-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v2/15/DQN_world_B100000.json
