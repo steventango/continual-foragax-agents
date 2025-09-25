75660888168cd0f149eb996fb00192b04a52c7d7
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-25 12:15:21 -0600

    Remove duplicate Search-Nearest baseline

--- experiments/E26-search-limited-fov/foragax/ForagaxTwoBiome-v2/slurm.sh
+++ experiments/E26-search-limited-fov/foragax/ForagaxTwoBiome-v2/slurm.sh
@@ -11,6 +11,5 @@ python scripts/slurm.py --cluster clusters/vulcan-cpu-30m.json --runs 30 --entry
 python scripts/slurm.py --cluster clusters/vulcan-cpu-30m.json --runs 30 --entry src/continuing_main.py --force -e experiments/E26-search-limited-fov/foragax/ForagaxTwoBiome-v2/Baselines/Search-Brown.json
 python scripts/slurm.py --cluster clusters/vulcan-cpu-3h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E26-search-limited-fov/foragax/ForagaxTwoBiome-v2/Baselines/Search-Morel-Avoid-Green.json
 python scripts/slurm.py --cluster clusters/vulcan-cpu-3h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E26-search-limited-fov/foragax/ForagaxTwoBiome-v2/Baselines/Search-Morel.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-30m.json --runs 30 --entry src/continuing_main.py --force -e experiments/E26-search-limited-fov/foragax/ForagaxTwoBiome-v2/Baselines/Search-Nearest.json
 python scripts/slurm.py --cluster clusters/vulcan-cpu-3h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E26-search-limited-fov/foragax/ForagaxTwoBiome-v2/Baselines/Search-Oracle.json
 python scripts/slurm.py --cluster clusters/vulcan-cpu-30m.json --runs 30 --entry src/continuing_main.py --force -e experiments/E26-search-limited-fov/foragax/ForagaxTwoBiome-v2/Baselines/Search-Oyster.json
