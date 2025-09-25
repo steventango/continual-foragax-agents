9a211b0ade22e01fb43a14e4501b24c1db55e41c
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-25 13:23:36 -0600

    Create E40 baselines

---
+++ experiments/E40-baselines/foragax/ForagaxTwoBiome-v4/process_data_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --account=aip-amw8
+#SBATCH --job-name=process_data_E40-baselines_foragax_ForagaxTwoBiome-v4
+#SBATCH --mem-per-cpu=128G
+#SBATCH --ntasks=1
+#SBATCH --output=../slurm-%j.out
+#SBATCH --time=01:00:00
+
+tar -xf venv.tar.xz -C $SLURM_TMPDIR
+
+export MPLBACKEND=TKAgg
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export NPROC=1
+export XLA_FLAGS="--xla_cpu_multi_thread_eigen=false intra_op_parallelism_threads=1"
+export XLA_PYTHON_CLIENT_MEM_FRACTION=0.95
+export JAX_PLATFORMS=cpu
+
+$SLURM_TMPDIR/.venv/bin/python src/process_data.py experiments/E40-baselines/foragax/ForagaxTwoBiome-v4
