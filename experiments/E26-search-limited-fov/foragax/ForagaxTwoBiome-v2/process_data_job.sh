949c6f877b0e982408a3e4926310a7510cb958d6
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-25 11:26:48 -0600

    New script for processing data

--- experiments/E26-search-limited-fov/foragax/ForagaxTwoBiome-v2/process_data_job.sh
+++ experiments/E26-search-limited-fov/foragax/ForagaxTwoBiome-v2/process_data_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=aip-amw8
-#SBATCH --job-name=learning_curve_E26-search-limited-fov_foragax_ForagaxTwoBiome-v2
+#SBATCH --job-name=process_data_E26-search-limited-fov_foragax_ForagaxTwoBiome-v2
 #SBATCH --mem-per-cpu=128G
 #SBATCH --ntasks=1
 #SBATCH --output=../slurm-%j.out
@@ -17,4 +17,4 @@ export XLA_FLAGS="--xla_cpu_multi_thread_eigen=false intra_op_parallelism_thread
 export XLA_PYTHON_CLIENT_MEM_FRACTION=0.95
 export JAX_PLATFORMS=cpu
 
-$SLURM_TMPDIR/.venv/bin/python experiments/E26-search-limited-fov/foragax/ForagaxTwoBiome-v2/learning_curve.py
+$SLURM_TMPDIR/.venv/bin/python src/process_data.py experiments/E26-search-limited-fov/foragax/ForagaxTwoBiome-v2
