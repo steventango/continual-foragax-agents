9bf6ec9361f43cb4874475ba5815bb68931e3708
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-25 17:45:40 -0600

    Create E45-mitigations

---
+++ experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/run.sh
@@ -0,0 +1,3 @@
+ln -s ../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v7/Baselines experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/Baselines
+mkdir -p results/E45-mitigations/foragax/ForagaxTwoBiome-v7
+ln -s ../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v7/Baselines results/E45-mitigations/foragax/ForagaxTwoBiome-v7/Baselines
