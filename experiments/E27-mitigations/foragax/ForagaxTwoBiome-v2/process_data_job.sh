967e17f00fc4263661faac374c1d05d34e4a3e8e
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-25 13:25:53 -0600

    Update with process data job

--- experiments/E27-mitigations/foragax/ForagaxTwoBiome-v2/process_data_job.sh
+++ experiments/E27-mitigations/foragax/ForagaxTwoBiome-v2/process_data_job.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #SBATCH --account=aip-amw8
-#SBATCH --job-name=learning_curve_a_E27-mitigations_foragax_ForagaxTwoBiome-v2
+#SBATCH --job-name=process_data_E27-mitigations_foragax_ForagaxTwoBiome-v2
 #SBATCH --mem-per-cpu=128G
-#SBATCH --exclude=rack08-11
+#SBATCH --ntasks=1
 #SBATCH --output=../slurm-%j.out
-#SBATCH --time=00:30:00
-
-module load arrow/19
+#SBATCH --time=01:00:00
 
 tar -xf venv.tar.xz -C $SLURM_TMPDIR
 
@@ -19,4 +17,4 @@ export XLA_FLAGS="--xla_cpu_multi_thread_eigen=false intra_op_parallelism_thread
 export XLA_PYTHON_CLIENT_MEM_FRACTION=0.95
 export JAX_PLATFORMS=cpu
 
-$SLURM_TMPDIR/.venv/bin/python experiments/E27-mitigations/foragax/ForagaxTwoBiome-v2/learning_curve_a.py
+$SLURM_TMPDIR/.venv/bin/python src/process_data.py experiments/E27-mitigations/foragax/ForagaxTwoBiome-v2
