cd24cdf049e03f292d2fcc50d13c422c9fac5ef1
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-25 13:49:21 -0600

    Update sync_results.sh torsync only parquet files

--- scripts/sync_results.sh
+++ scripts/sync_results.sh
@@ -1,5 +1,4 @@
-# rsync -azP --exclude '*/sweep/*' vulcan:/home/stang5/scratch/continual-foragax-agents/results .
-rsync -azP vulcan:/home/stang5/scratch/continual-foragax-agents/results/E39-baselines/foragax/ForagaxTwoBiome-v3/ results/E39-baselines/foragax
+rsync -azP --include='*.parquet' --exclude='*' vulcan:/home/stang5/scratch/continual-foragax-agents/results .
 # TODO: change the lop paths to be identical to ours so it is easier to sync
 # move to top level data/results/...
 # rsync -azP ../continual-foragax-loss-of-plasticity/lop/rl/data/foragax/ForagaxWeather-v1/ForagaxWeather-v1-15/PPO/data ./results/foragax/ForagaxWeather-v1/ForagaxWeather-v1-15/PPO
