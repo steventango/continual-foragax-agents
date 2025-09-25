57537709b87b22a7d9a643ba9f663632ff7779c6
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-25 14:14:11 -0600

    Fix rsync of data files with directories

--- scripts/sync_results.sh
+++ scripts/sync_results.sh
@@ -1,4 +1,4 @@
-rsync -azP --include='*.parquet' --exclude='*' vulcan:/home/stang5/scratch/continual-foragax-agents/results .
+rsync -azP --include='*/' --include='*.parquet' --exclude='*' vulcan:/home/stang5/scratch/continual-foragax-agents/results .
 # TODO: change the lop paths to be identical to ours so it is easier to sync
 # move to top level data/results/...
 # rsync -azP ../continual-foragax-loss-of-plasticity/lop/rl/data/foragax/ForagaxWeather-v1/ForagaxWeather-v1-15/PPO/data ./results/foragax/ForagaxWeather-v1/ForagaxWeather-v1-15/PPO
