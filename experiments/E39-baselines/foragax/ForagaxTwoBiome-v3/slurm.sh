48c496929197b0b146c11909cab520fb9fdda0f0
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-25 12:37:37 -0600

    Create E39-baselines

---
+++ experiments/E39-baselines/foragax/ForagaxTwoBiome-v3/slurm.sh
@@ -0,0 +1,8 @@
+
+python scripts/slurm.py --cluster clusters/vulcan-cpu-15m.json --runs 30 --entry src/continuing_main.py --force -e experiments/E39-baselines/foragax/ForagaxTwoBiome-v3/Baselines/Random.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-30m.json --runs 30 --entry src/continuing_main.py --force -e experiments/E39-baselines/foragax/ForagaxTwoBiome-v3/Baselines/Search-Brown-Avoid-Green.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-30m.json --runs 30 --entry src/continuing_main.py --force -e experiments/E39-baselines/foragax/ForagaxTwoBiome-v3/Baselines/Search-Brown.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-3h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E39-baselines/foragax/ForagaxTwoBiome-v3/Baselines/Search-Morel-Avoid-Green.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-3h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E39-baselines/foragax/ForagaxTwoBiome-v3/Baselines/Search-Morel.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-3h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E39-baselines/foragax/ForagaxTwoBiome-v3/Baselines/Search-Oracle.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-30m.json --runs 30 --entry src/continuing_main.py --force -e experiments/E39-baselines/foragax/ForagaxTwoBiome-v3/Baselines/Search-Oyster.json
