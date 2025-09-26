de2591c77bb4ea027c852ad1dc7f34bc9dd2230a
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-26 08:58:31 -0600

    Fix symlinks

--- experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/run.sh
+++ experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/run.sh
@@ -1,6 +1,8 @@
 ln -s ../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v7/Baselines experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/Baselines
 ln -s ../../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v7/9/DQN.json experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN.json
+ln -s ../../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v7/9/DQN.json experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN_frozen.json
 mkdir -p results/E45-mitigations/foragax/ForagaxTwoBiome-v7
 ln -s ../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v7/Baselines results/E45-mitigations/foragax/ForagaxTwoBiome-v7/Baselines
 mkdir -p results/E45-mitigations/foragax/ForagaxTwoBiome-v7/9
-ln -s ../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v7/9/DQN.json results/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN.json
+ln -s ../../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v7/9/DQN results/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN
+ln -s ../../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v7/9/DQN_frozen results/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN_frozen
