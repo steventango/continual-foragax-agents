9268db20cf0414134f78a4660da1326972283155
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-25 22:36:07 -0600

    Use 10h jobs

--- experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/slurm.sh
+++ experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/slurm.sh
@@ -1,6 +1,6 @@
-python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN_Hare_and_Tortoise.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-9h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN_L2_Init.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-9h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN_LN.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN_Reset_Head.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN_Shrink_and_Perturb.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-10h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN_Hare_and_Tortoise.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-10h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN_L2_Init.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-10h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN_LN.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-10h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN_Reset_Head.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-10h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN_Shrink_and_Perturb.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-10h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN.json
