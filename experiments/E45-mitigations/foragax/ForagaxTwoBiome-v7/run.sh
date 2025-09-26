f02bf2eff6ab253707c5ad0cd4f9e423755972f9
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-25 22:42:47 -0600

    Add symbolic links and directories for DQN.json in E45-mitigations setup

--- experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/run.sh
+++ experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/run.sh
@@ -1,3 +1,6 @@
 ln -s ../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v7/Baselines experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/Baselines
+ln -s ../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v7/9/DQN.json experiments/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN.json
 mkdir -p results/E45-mitigations/foragax/ForagaxTwoBiome-v7
 ln -s ../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v7/Baselines results/E45-mitigations/foragax/ForagaxTwoBiome-v7/Baselines
+mkdir -p results/E45-mitigations/foragax/ForagaxTwoBiome-v7/9
+ln -s ../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v7/9/DQN.json results/E45-mitigations/foragax/ForagaxTwoBiome-v7/9/DQN.json
