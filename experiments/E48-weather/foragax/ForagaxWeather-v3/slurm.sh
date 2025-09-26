1f1352b7e1aca57aab893eeb32600e30463f18ee
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-09-25 20:46:04 -0600

    Update slurm.sh to correct cluster configuration for Search-Nearest baseline

--- experiments/E48-weather/foragax/ForagaxWeather-v3/slurm.sh
+++ experiments/E48-weather/foragax/ForagaxWeather-v3/slurm.sh
@@ -1,5 +1,5 @@
 python scripts/slurm.py --cluster clusters/vulcan-cpu-8h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E48-weather/foragax/ForagaxWeather-v3/9/DQN.json
 python scripts/slurm.py --cluster clusters/vulcan-cpu-1h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E48-weather/foragax/ForagaxWeather-v3/9/DQN_frozen.json
 
-python scripts/slurm.py --cluster clusters/vulcan-cpu-1h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E48-weather/foragax/ForagaxWeather-v3/Baselines/Search-Nearest.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-15m.json --runs 30 --entry src/continuing_main.py --force -e experiments/E48-weather/foragax/ForagaxWeather-v3/Baselines/Search-Nearest.json
 python scripts/slurm.py --cluster clusters/vulcan-cpu-3h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E48-weather/foragax/ForagaxWeather-v3/Baselines/Search-Oracle.json
