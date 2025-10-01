b5cc5a331b3a9265d3b40d219289ccf9bb7fbf01
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-01 15:34:41 -0600

    Use fir

--- experiments/E71-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10/slurm.sh
+++ experiments/E71-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10/slurm.sh
@@ -1,2 +1,2 @@
-python scripts/slurm.py --cluster clusters/vulcan-gpu-mps-6h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E71-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10/15/DQN.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-mps-6h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E71-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10/15/DQN_world.json
+python scripts/slurm.py --cluster clusters/fir-gpu-mps-6h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E71-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10/15/DQN.json
+python scripts/slurm.py --cluster clusters/fir-gpu-mps-6h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E71-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10/15/DQN_world.json
