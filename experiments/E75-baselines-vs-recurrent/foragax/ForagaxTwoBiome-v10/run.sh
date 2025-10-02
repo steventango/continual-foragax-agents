5a51206b8dda0a81f9069a12d5cc19f4a726f7ef
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-02 09:21:19 -0600

    Update symbolic links

--- experiments/E75-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10/run.sh
+++ experiments/E75-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10/run.sh
@@ -1,3 +1,3 @@
-ln -s ../../../E69-search-limited-fov/foragax/ForagaxTwoBiome-v10/Baselines experiments/E75-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10/Baselines
+ln -s ../../../73-search-limited-fov/foragax/ForagaxTwoBiome-v10/Baselines experiments/E75-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10/Baselines
 mkdir -p results/E75-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10
-ln -s ../../../E69-search-limited-fov/foragax/ForagaxTwoBiome-v10/Baselines results/E75-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10/Baselines
+ln -s ../../../73-search-limited-fov/foragax/ForagaxTwoBiome-v10/Baselines results/E75-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10/Baselines
