365e8f92ac3fe609a908e1803cb0fccaa9932fc2
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-02 00:04:35 -0600

    Fix symlinks

--- experiments/E70-mitigations/foragax/ForagaxTwoBiome-v10/run.sh
+++ experiments/E70-mitigations/foragax/ForagaxTwoBiome-v10/run.sh
@@ -1,8 +1,8 @@
-ln -s ../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v10/Baselines experiments/E70-mitigations/foragax/ForagaxTwoBiome-v10/Baselines
-ln -s ../../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN.json experiments/E70-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN.json
-ln -s ../../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN.json experiments/E70-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN_frozen.json
+ln -s ../../../E69-search-limited-fov/foragax/ForagaxTwoBiome-v10/Baselines experiments/E70-mitigations/foragax/ForagaxTwoBiome-v10/Baselines
+ln -s ../../../../E69-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN.json experiments/E70-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN.json
+ln -s ../../../../E69-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN.json experiments/E70-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN_frozen.json
 mkdir -p results/E70-mitigations/foragax/ForagaxTwoBiome-v10
-ln -s ../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v10/Baselines results/E70-mitigations/foragax/ForagaxTwoBiome-v10/Baselines
+ln -s ../../../E69-search-limited-fov/foragax/ForagaxTwoBiome-v10/Baselines results/E70-mitigations/foragax/ForagaxTwoBiome-v10/Baselines
 mkdir -p results/E70-mitigations/foragax/ForagaxTwoBiome-v10/9
-ln -s ../../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN results/E70-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN
-ln -s ../../../../E44-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN_frozen results/E70-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN_frozen
+ln -s ../../../../E69-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN results/E70-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN
+ln -s ../../../../E69-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN_frozen results/E70-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN_frozen
