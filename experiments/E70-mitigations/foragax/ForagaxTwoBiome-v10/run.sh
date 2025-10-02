ed4bf7969b87fb03edd0a49b5634d2a3a45a6b36
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-02 08:53:15 -0600

    Plot results

--- experiments/E70-mitigations/foragax/ForagaxTwoBiome-v10/run.sh
+++ experiments/E70-mitigations/foragax/ForagaxTwoBiome-v10/run.sh
@@ -1,6 +1,6 @@
 ln -s ../../../E69-search-limited-fov/foragax/ForagaxTwoBiome-v10/Baselines experiments/E70-mitigations/foragax/ForagaxTwoBiome-v10/Baselines
 ln -s ../../../../E69-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN.json experiments/E70-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN.json
-ln -s ../../../../E69-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN.json experiments/E70-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN_frozen.json
+ln -s ../../../../E69-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN_frozen.json experiments/E70-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN_frozen.json
 mkdir -p results/E70-mitigations/foragax/ForagaxTwoBiome-v10
 ln -s ../../../E69-search-limited-fov/foragax/ForagaxTwoBiome-v10/Baselines results/E70-mitigations/foragax/ForagaxTwoBiome-v10/Baselines
 mkdir -p results/E70-mitigations/foragax/ForagaxTwoBiome-v10/9
