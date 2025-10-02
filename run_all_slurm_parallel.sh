a50a84a69560789691b3c0c0236fba979b34406b
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-02 14:13:41 -0600

    Run everything else

--- run_all_slurm_parallel.sh
+++ run_all_slurm_parallel.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# Run all slurm_search.sh scripts in parallel
-bash experiments/E73-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v10/slurm.sh &
-bash experiments/E76-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v13/slurm.sh &
-bash experiments/E79-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v14/slurm.sh &
-bash experiments/E82-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v15/slurm.sh &
-bash experiments/E85-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v16/slurm.sh &
+# Run all slurm.sh scripts in parallel
+# bash experiments/E73-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v10/slurm.sh &
+bash experiments/E74-mitigations/foragax-sweep/ForagaxTwoBiome-v10/slurm.sh &
+bash experiments/E75-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v10/slurm.sh &
+# bash experiments/E76-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v13/slurm.sh &
+bash experiments/E77-mitigations/foragax-sweep/ForagaxTwoBiome-v13/slurm.sh &
+bash experiments/E78-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v13/slurm.sh &
+# bash experiments/E79-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v14/slurm.sh &
+bash experiments/E80-mitigations/foragax-sweep/ForagaxTwoBiome-v14/slurm.sh &
+bash experiments/E81-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v14/slurm.sh &
+# bash experiments/E82-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v15/slurm.sh &
+bash experiments/E83-mitigations/foragax-sweep/ForagaxTwoBiome-v15/slurm.sh &
+bash experiments/E84-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v15/slurm.sh &
+# bash experiments/E85-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v16/slurm.sh &
+bash experiments/E86-mitigations/foragax-sweep/ForagaxTwoBiome-v16/slurm.sh &
+bash experiments/E87-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v16/slurm.sh &
 
 # Wait for all background jobs to complete
 wait
 
-echo "All slurm_search.sh scripts have been executed."
+echo "All slurm.sh scripts have been executed."
