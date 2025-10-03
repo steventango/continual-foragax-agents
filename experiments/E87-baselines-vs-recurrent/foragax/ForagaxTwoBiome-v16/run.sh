994af1126f806f6857d8a7c2c0d0cb042ff841f0
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-03 13:15:48 -0600

    Fix typos

--- experiments/E87-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v16/run.sh
+++ experiments/E87-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v16/run.sh
@@ -1,3 +1,3 @@
-ln -s ../../../73-search-limited-fov/foragax/ForagaxTwoBiome-v16/Baselines experiments/E87-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v16/Baselines
+ln -s ../../../E84-search-limited-fov/foragax/ForagaxTwoBiome-v16/Baselines experiments/E87-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v16/Baselines
 mkdir -p results/E87-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v16
-ln -s ../../../73-search-limited-fov/foragax/ForagaxTwoBiome-v16/Baselines results/E87-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v16/Baselines
+ln -s ../../../E84-search-limited-fov/foragax/ForagaxTwoBiome-v16/Baselines results/E87-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v16/Baselines
