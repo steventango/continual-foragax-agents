7aadb69414ac6bf05cf7d3da99343372c5ef4805
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-02 19:01:44 -0600

    Edit times

--- experiments/E86-mitigations/foragax-sweep/ForagaxTwoBiome-v16/slurm.sh
+++ experiments/E86-mitigations/foragax-sweep/ForagaxTwoBiome-v16/slurm.sh
@@ -1,5 +1,5 @@
 python scripts/slurm.py --cluster clusters/vulcan-cpu-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E86-mitigations/foragax-sweep/ForagaxTwoBiome-v16/9/DQN_Hare_and_Tortoise.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E86-mitigations/foragax-sweep/ForagaxTwoBiome-v16/9/DQN_L2_Init.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E86-mitigations/foragax-sweep/ForagaxTwoBiome-v16/9/DQN_LN.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E86-mitigations/foragax-sweep/ForagaxTwoBiome-v16/9/DQN_L2_Init.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E86-mitigations/foragax-sweep/ForagaxTwoBiome-v16/9/DQN_LN.json
 python scripts/slurm.py --cluster clusters/vulcan-cpu-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E86-mitigations/foragax-sweep/ForagaxTwoBiome-v16/9/DQN_Reset_Head.json
 python scripts/slurm.py --cluster clusters/vulcan-cpu-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E86-mitigations/foragax-sweep/ForagaxTwoBiome-v16/9/DQN_Shrink_and_Perturb.json
