aabcc14adc7ff4ec8a44ec2575771598a59eeeb8
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-03 13:46:58 -0600

    Unify learning_curve scripts

---
+++ scripts/run_all_plot.sh
@@ -0,0 +1,16 @@
+python src/learning_curve.py experiments/E73-search-limited-fov/foragax/ForagaxTwoBiome-v10 &
+python src/learning_curve.py experiments/E74-mitigations/foragax/ForagaxTwoBiome-v10 &
+python src/learning_curve.py experiments/E75-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10 &
+python src/learning_curve.py experiments/E76-search-limited-fov/foragax/ForagaxTwoBiome-v13 &
+python src/learning_curve.py experiments/E77-mitigations/foragax/ForagaxTwoBiome-v13 &
+python src/learning_curve.py experiments/E78-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v13 &
+python src/learning_curve.py experiments/E79-search-limited-fov/foragax/ForagaxTwoBiome-v14 &
+python src/learning_curve.py experiments/E80-mitigations/foragax/ForagaxTwoBiome-v14 &
+python src/learning_curve.py experiments/E81-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v14 &
+python src/learning_curve.py experiments/E82-search-limited-fov/foragax/ForagaxTwoBiome-v15 &
+python src/learning_curve.py experiments/E83-mitigations/foragax/ForagaxTwoBiome-v15 &
+python src/learning_curve.py experiments/E84-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v15 &
+python src/learning_curve.py experiments/E85-search-limited-fov/foragax/ForagaxTwoBiome-v16 &
+python src/learning_curve.py experiments/E86-mitigations/foragax/ForagaxTwoBiome-v16 &
+python src/learning_curve.py experiments/E87-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v16 &
+wait
