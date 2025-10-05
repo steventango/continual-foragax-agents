ddcf3aad1818bae760467fa0ef7604ce6afb3f2f
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-05 09:22:32 -0600

    Run 5 seeds

--- experiments/E96-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v13/slurm.sh
+++ experiments/E96-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v13/slurm.sh
@@ -1,1 +1,1 @@
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 3 --entry src/continuing_main.py --force -e experiments/E96-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v13/9/DQN.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E96-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v13/9/DQN.json
