508d79e5c199dbc3030d1f9a5912065e4ee68b82
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-05 14:20:01 -0600

    Create run_all_plot_sweep

---
+++ scripts/run_all_plot_sweep.sh
@@ -0,0 +1,27 @@
+# python src/learning_curve.py experiments/E92-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v10 &
+# python src/learning_curve.py experiments/E93-mitigations/foragax-sweep/ForagaxTwoBiome-v10 &
+# python src/learning_curve.py experiments/E94-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v10 &
+# python src/learning_curve.py experiments/E95-weather/foragax-sweep/ForagaxWeather-v4 &
+# python src/learning_curve.py experiments/E96-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v13 &
+# python src/learning_curve.py experiments/E97-mitigations/foragax-sweep/ForagaxTwoBiome-v13 &
+# python src/learning_curve.py experiments/E98-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v13 &
+# python src/learning_curve.py experiments/E99-weather/foragax-sweep/ForagaxWeather-v5 &
+
+# python src/mushroom_curve.py experiments/E92-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v10 &
+# python src/mushroom_curve.py experiments/E93-mitigations/foragax-sweep/ForagaxTwoBiome-v10 &
+# python src/mushroom_curve.py experiments/E94-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v10 &
+# python src/mushroom_curve.py experiments/E95-weather/foragax-sweep/ForagaxWeather-v4 &
+# python src/mushroom_curve.py experiments/E96-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v13 &
+# python src/mushroom_curve.py experiments/E97-mitigations/foragax-sweep/ForagaxTwoBiome-v13 &
+# python src/mushroom_curve.py experiments/E98-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v13 &
+# python src/mushroom_curve.py experiments/E99-weather/foragax-sweep/ForagaxWeather-v5 &
+
+# python src/biome_curve.py experiments/E92-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v10 &
+# python src/biome_curve.py experiments/E93-mitigations/foragax-sweep/ForagaxTwoBiome-v10 &
+# python src/biome_curve.py experiments/E94-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v10 &
+# python src/biome_curve.py experiments/E95-weather/foragax-sweep/ForagaxWeather-v4 &
+# python src/biome_curve.py experiments/E96-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v13 &
+# python src/biome_curve.py experiments/E97-mitigations/foragax-sweep/ForagaxTwoBiome-v13 &
+# python src/biome_curve.py experiments/E98-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v13 &
+# python src/biome_curve.py experiments/E99-weather/foragax-sweep/ForagaxWeather-v5 &
+wait
