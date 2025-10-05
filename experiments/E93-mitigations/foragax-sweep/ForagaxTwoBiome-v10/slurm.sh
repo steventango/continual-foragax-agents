78194970f9f2d232bd5293acae3952ec3e78e060
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-05 01:01:02 -0600

    Edit slurm.sh

--- experiments/E93-mitigations/foragax-sweep/ForagaxTwoBiome-v10/slurm.sh
+++ experiments/E93-mitigations/foragax-sweep/ForagaxTwoBiome-v10/slurm.sh
@@ -1,5 +1,6 @@
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E93-mitigations/foragax-sweep/ForagaxTwoBiome-v10/9/DQN_CReLU.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E93-mitigations/foragax-sweep/ForagaxTwoBiome-v10/9/DQN_Hare_and_Tortoise.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E93-mitigations/foragax-sweep/ForagaxTwoBiome-v10/9/DQN_L2.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E93-mitigations/foragax-sweep/ForagaxTwoBiome-v10/9/DQN_L2_Init.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E93-mitigations/foragax-sweep/ForagaxTwoBiome-v10/9/DQN_LN.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E93-mitigations/foragax-sweep/ForagaxTwoBiome-v10/9/DQN_Reset_Head.json
--- experiments/E99-weather/foragax-sweep/ForagaxTwoBiome-v5/slurm.sh
+++ experiments/E99-weather/foragax-sweep/ForagaxTwoBiome-v5/slurm.sh
@@ -1,6 +1,7 @@
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E99-weather/foragax-sweep/ForagaxTwoBiome-v5/9/DQN.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E99-weather/foragax-sweep/ForagaxTwoBiome-v5/9/DQN_CReLU.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E99-weather/foragax-sweep/ForagaxTwoBiome-v5/9/DQN_Hare_and_Tortoise.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E99-weather/foragax-sweep/ForagaxTwoBiome-v5/9/DQN_L2.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E99-weather/foragax-sweep/ForagaxTwoBiome-v5/9/DQN_L2_Init.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E99-weather/foragax-sweep/ForagaxTwoBiome-v5/9/DQN_LN.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E99-weather/foragax-sweep/ForagaxTwoBiome-v5/9/DQN_Reset_Head.json
