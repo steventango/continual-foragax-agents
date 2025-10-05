57a7b36127dc39eb4e7781158b248f012a81f581
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-05 09:22:06 -0600

    Plot sweeps

--- scripts/run_all_plot.sh
+++ scripts/run_all_plot.sh
@@ -1,32 +1,21 @@
-python src/learning_curve.py experiments/E73-search-limited-fov/foragax/ForagaxTwoBiome-v10 --save-type jpg &
-python src/learning_curve.py experiments/E74-mitigations/foragax/ForagaxTwoBiome-v10 --save-type jpg &
-python src/learning_curve.py experiments/E75-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10 --save-type jpg &
-python src/learning_curve.py experiments/E76-search-limited-fov/foragax/ForagaxTwoBiome-v13 --save-type jpg &
-python src/learning_curve.py experiments/E77-mitigations/foragax/ForagaxTwoBiome-v13 --save-type jpg &
-python src/learning_curve.py experiments/E78-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v13 --save-type jpg &
-python src/learning_curve.py experiments/E79-search-limited-fov/foragax/ForagaxTwoBiome-v14 --save-type jpg &
-python src/learning_curve.py experiments/E80-mitigations/foragax/ForagaxTwoBiome-v14 --save-type jpg &
-python src/learning_curve.py experiments/E81-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v14 --save-type jpg &
-python src/learning_curve.py experiments/E82-search-limited-fov/foragax/ForagaxTwoBiome-v15 --save-type jpg &
-python src/learning_curve.py experiments/E83-mitigations/foragax/ForagaxTwoBiome-v15 --save-type jpg &
-python src/learning_curve.py experiments/E84-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v15 --save-type jpg &
-python src/learning_curve.py experiments/E85-search-limited-fov/foragax/ForagaxTwoBiome-v16 --save-type jpg &
-python src/learning_curve.py experiments/E86-mitigations/foragax/ForagaxTwoBiome-v16 --save-type jpg &
-python src/learning_curve.py experiments/E87-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v16 --save-type jpg &
+python src/learning_curve.py experiments/E92-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v10 &
+python src/learning_curve.py experiments/E93-mitigations/foragax-sweep/ForagaxTwoBiome-v10 &
+python src/learning_curve.py experiments/E94-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v10 &
+python src/learning_curve.py experiments/E96-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v13 &
+python src/learning_curve.py experiments/E97-mitigations/foragax-sweep/ForagaxTwoBiome-v13 &
+python src/learning_curve.py experiments/E98-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v13 &
 wait
-python src/mushroom_curve.py experiments/E73-search-limited-fov/foragax/ForagaxTwoBiome-v10 --trace-exponent 1 --save-type jpg &
-python src/mushroom_curve.py experiments/E74-mitigations/foragax/ForagaxTwoBiome-v10 --trace-exponent 1 --save-type jpg &
-python src/mushroom_curve.py experiments/E75-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v10 --trace-exponent 1 --save-type jpg &
-python src/mushroom_curve.py experiments/E76-search-limited-fov/foragax/ForagaxTwoBiome-v13 --trace-exponent 1 --save-type jpg &
-python src/mushroom_curve.py experiments/E77-mitigations/foragax/ForagaxTwoBiome-v13 --trace-exponent 1 --save-type jpg &
-python src/mushroom_curve.py experiments/E78-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v13 --trace-exponent 1 --save-type jpg &
-python src/mushroom_curve.py experiments/E79-search-limited-fov/foragax/ForagaxTwoBiome-v14 --trace-exponent 1 --save-type jpg &
-python src/mushroom_curve.py experiments/E80-mitigations/foragax/ForagaxTwoBiome-v14 --trace-exponent 1 --save-type jpg &
-python src/mushroom_curve.py experiments/E81-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v14 --trace-exponent 1 --save-type jpg &
-python src/mushroom_curve.py experiments/E82-search-limited-fov/foragax/ForagaxTwoBiome-v15 --trace-exponent 1 --save-type jpg &
-python src/mushroom_curve.py experiments/E83-mitigations/foragax/ForagaxTwoBiome-v15 --trace-exponent 1 --save-type jpg &
-python src/mushroom_curve.py experiments/E84-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v15 --trace-exponent 1 --save-type jpg &
-python src/mushroom_curve.py experiments/E85-search-limited-fov/foragax/ForagaxTwoBiome-v16 --trace-exponent 1 --save-type jpg &
-python src/mushroom_curve.py experiments/E86-mitigations/foragax/ForagaxTwoBiome-v16 --trace-exponent 1 --save-type jpg &
-python src/mushroom_curve.py experiments/E87-baselines-vs-recurrent/foragax/ForagaxTwoBiome-v16 --trace-exponent 1 --save-type jpg &
+python src/mushroom_curve.py experiments/E92-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v10 &
+python src/mushroom_curve.py experiments/E93-mitigations/foragax-sweep/ForagaxTwoBiome-v10 &
+python src/mushroom_curve.py experiments/E94-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v10 &
+python src/mushroom_curve.py experiments/E96-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v13 &
+python src/mushroom_curve.py experiments/E97-mitigations/foragax-sweep/ForagaxTwoBiome-v13 &
+python src/mushroom_curve.py experiments/E98-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v13 &
+wait
+python src/biome_curve.py experiments/E92-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v10 &
+python src/biome_curve.py experiments/E93-mitigations/foragax-sweep/ForagaxTwoBiome-v10 &
+python src/biome_curve.py experiments/E94-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v10 &
+python src/biome_curve.py experiments/E96-search-limited-fov/foragax-sweep/ForagaxTwoBiome-v13 &
+python src/biome_curve.py experiments/E97-mitigations/foragax-sweep/ForagaxTwoBiome-v13 &
+python src/biome_curve.py experiments/E98-baselines-vs-recurrent/foragax-sweep/ForagaxTwoBiome-v13 &
 wait
