ec856780ca0cfc6749bbf95cd9153ded2b45446f
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-05 14:16:05 -0600

    Fix symbolic links

--- experiments/E93-mitigations/foragax/ForagaxTwoBiome-v10/run.sh
+++ experiments/E93-mitigations/foragax/ForagaxTwoBiome-v10/run.sh
@@ -1,8 +1,10 @@
 ln -s ../../../E92-search-limited-fov/foragax/ForagaxTwoBiome-v10/Baselines experiments/E93-mitigations/foragax/ForagaxTwoBiome-v10/Baselines
 ln -s ../../../../E92-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN.json experiments/E93-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN.json
-ln -s ../../../../E92-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN_frozen.json experiments/E93-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN_frozen.json
+ln -s ../../../../E92-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN_frozen_1M.json experiments/E93-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN_frozen_1M.json
+ln -s ../../../../E92-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN_frozen_5M.json experiments/E93-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN_frozen_5M.json
 mkdir -p results/E93-mitigations/foragax/ForagaxTwoBiome-v10
 ln -s ../../../E92-search-limited-fov/foragax/ForagaxTwoBiome-v10/Baselines results/E93-mitigations/foragax/ForagaxTwoBiome-v10/Baselines
 mkdir -p results/E93-mitigations/foragax/ForagaxTwoBiome-v10/9
 ln -s ../../../../E92-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN results/E93-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN
-ln -s ../../../../E92-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN_frozen results/E93-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN_frozen
+ln -s ../../../../E92-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN_frozen_1M results/E93-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN_frozen_1M
+ln -s ../../../../E92-search-limited-fov/foragax/ForagaxTwoBiome-v10/9/DQN_frozen_5M results/E93-mitigations/foragax/ForagaxTwoBiome-v10/9/DQN_frozen_5M
--- experiments/E97-mitigations/foragax/ForagaxTwoBiome-v13/run.sh
+++ experiments/E97-mitigations/foragax/ForagaxTwoBiome-v13/run.sh
@@ -1,8 +1,10 @@
 ln -s ../../../E96-search-limited-fov/foragax/ForagaxTwoBiome-v13/Baselines experiments/E97-mitigations/foragax/ForagaxTwoBiome-v13/Baselines
 ln -s ../../../../E96-search-limited-fov/foragax/ForagaxTwoBiome-v13/9/DQN.json experiments/E97-mitigations/foragax/ForagaxTwoBiome-v13/9/DQN.json
-ln -s ../../../../E96-search-limited-fov/foragax/ForagaxTwoBiome-v13/9/DQN_frozen.json experiments/E97-mitigations/foragax/ForagaxTwoBiome-v13/9/DQN_frozen.json
+ln -s ../../../../E96-search-limited-fov/foragax/ForagaxTwoBiome-v13/9/DQN_frozen_1M.json experiments/E97-mitigations/foragax/ForagaxTwoBiome-v13/9/DQN_frozen_1M.json
+ln -s ../../../../E96-search-limited-fov/foragax/ForagaxTwoBiome-v13/9/DQN_frozen_5M.json experiments/E97-mitigations/foragax/ForagaxTwoBiome-v13/9/DQN_frozen_5M.json
 mkdir -p results/E97-mitigations/foragax/ForagaxTwoBiome-v13
 ln -s ../../../E96-search-limited-fov/foragax/ForagaxTwoBiome-v13/Baselines results/E97-mitigations/foragax/ForagaxTwoBiome-v13/Baselines
 mkdir -p results/E97-mitigations/foragax/ForagaxTwoBiome-v13/9
 ln -s ../../../../E96-search-limited-fov/foragax/ForagaxTwoBiome-v13/9/DQN results/E97-mitigations/foragax/ForagaxTwoBiome-v13/9/DQN
-ln -s ../../../../E96-search-limited-fov/foragax/ForagaxTwoBiome-v13/9/DQN_frozen results/E97-mitigations/foragax/ForagaxTwoBiome-v13/9/DQN_frozen
+ln -s ../../../../E96-search-limited-fov/foragax/ForagaxTwoBiome-v13/9/DQN_frozen_1M results/E97-mitigations/foragax/ForagaxTwoBiome-v13/9/DQN_frozen_1M
+ln -s ../../../../E96-search-limited-fov/foragax/ForagaxTwoBiome-v13/9/DQN_frozen_5M results/E97-mitigations/foragax/ForagaxTwoBiome-v13/9/DQN_frozen_5M
