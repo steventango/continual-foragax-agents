f41a4f4b57faa67384ca689887b60183aa4cfc0b
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-06 18:50:48 -0600

    Double memory

--- experiments/E96-search-limited-fov/foragax/ForagaxTwoBiome-v13/process_data_job.sh
+++ experiments/E96-search-limited-fov/foragax/ForagaxTwoBiome-v13/process_data_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=aip-amw8
 #SBATCH --job-name=E96_process_data-search-limited-fov_foragax_ForagaxTwoBiome-v13
-#SBATCH --mem-per-cpu=32G
+#SBATCH --mem-per-cpu=64G
 #SBATCH --ntasks=4
 #SBATCH --output=../slurm-%j.out
 #SBATCH --time=01:00:00
