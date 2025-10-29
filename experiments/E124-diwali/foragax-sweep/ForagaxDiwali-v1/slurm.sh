563574674fd813cf1df44dd86cb861988fad3dde
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-28 18:05:39 -0600

    Create E124

---
+++ experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1/slurm.sh
@@ -0,0 +1,1 @@
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1/9/DQN.json
---
+++ experiments/E124-diwali/local.sh
@@ -0,0 +1,2 @@
+python scripts/local.py --runs 1 --entry src/continuing_main.py -e experiments/E124-diwali/foragax/ForagaxDiwali-v1/Baselines/*.json
+python src/process_data.py experiments/E124-diwali/foragax/ForagaxDiwali-v1
