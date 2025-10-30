4b305ecfca1789a631f4b15c209a64c78af237b9
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-30 14:51:55 -0600

    Increase to 1h

--- experiments/E123-two-biome/foragax/ForagaxSineTwoBiome-v1/slurm_search.sh
+++ experiments/E123-two-biome/foragax/ForagaxSineTwoBiome-v1/slurm_search.sh
@@ -1,3 +1,3 @@
-python scripts/slurm.py --cluster clusters/vulcan-cpu-30m.json --runs 30 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax/ForagaxSineTwoBiome-v1/Baselines/Search-Brown-Avoid-Green.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-30m.json --runs 30 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax/ForagaxSineTwoBiome-v1/Baselines/Search-Nearest.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-1h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax/ForagaxSineTwoBiome-v1/Baselines/Search-Brown-Avoid-Green.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-1h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax/ForagaxSineTwoBiome-v1/Baselines/Search-Nearest.json
 python scripts/slurm.py --cluster clusters/vulcan-cpu-3h.json --runs 30 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax/ForagaxSineTwoBiome-v1/Baselines/Search-Oracle.json
