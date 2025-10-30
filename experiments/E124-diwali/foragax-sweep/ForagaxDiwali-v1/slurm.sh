bbe68a8942003f711bf6c4c4688d717d251e603b
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-30 09:36:20 -0600

    Adjust timing

--- experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1/slurm.sh
+++ experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1/slurm.sh
@@ -1,5 +1,5 @@
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-4h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1/9/DQN.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-4h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1/15/DQN.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1/9/DQN.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1/15/DQN.json
 
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-4h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1/9/DQN_L2.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-4h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1/9/DQN_SWR.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1/9/DQN_L2.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-3h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1/9/DQN_SWR.json
