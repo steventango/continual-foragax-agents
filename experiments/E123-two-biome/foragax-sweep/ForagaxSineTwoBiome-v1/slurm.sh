ff88d9a7759a95f80d20abc25344f70a7f337273
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-29 21:37:14 -0600

    Add slurm script

--- experiments/E123-two-biome/foragax-sweep/ForagaxSineTwoBiome-v1/slurm.sh
+++ experiments/E123-two-biome/foragax-sweep/ForagaxSineTwoBiome-v1/slurm.sh
@@ -1,1 +1,12 @@
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax-sweep/ForagaxSineTwoBiome-v1/5/DQN.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax-sweep/ForagaxSineTwoBiome-v1/7/DQN.json
 python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax-sweep/ForagaxSineTwoBiome-v1/9/DQN.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax-sweep/ForagaxSineTwoBiome-v1/15/DQN.json
+
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax-sweep/ForagaxSineTwoBiome-v1/5/DQN_L2.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax-sweep/ForagaxSineTwoBiome-v1/7/DQN_L2.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax-sweep/ForagaxSineTwoBiome-v1/9/DQN_L2.json
+
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax-sweep/ForagaxSineTwoBiome-v1/5/DQN_SWR.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax-sweep/ForagaxSineTwoBiome-v1/7/DQN_SWR.json
+python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-1h.json --runs 5 --entry src/continuing_main.py --force -e experiments/E123-two-biome/foragax-sweep/ForagaxSineTwoBiome-v1/9/DQN_SWR.json
