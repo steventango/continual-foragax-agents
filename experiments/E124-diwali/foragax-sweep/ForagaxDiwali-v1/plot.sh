c2a6c8a2ed641f01f5b1fd4966bc254be8f3417f
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-30 21:59:29 -0600

    Plot sweep results

--- experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1/plot.sh
+++ experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1/plot.sh
@@ -1,2 +1,2 @@
-python src/learning_curve.py experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1 --plot-name fig1a_diwali_sweep --filter-alg-apertures DQN:5 PPO:5 --ylim "-0.05" 0.25
-python src/learning_curve.py experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1 --plot-name fig1a_diwali_sweep_seeds --filter-alg-apertures DQN:5 PPO:5 --ylim "-0.1" 0.25 --subplot-by-seed
+python src/learning_curve.py experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1 --plot-name fig1a_diwali_sweep --filter-alg-apertures DQN:5 PPO:5 PPO-RTU:5 DQN:9 PPO:9 PPO-RTU:9 --ylim "-0.05" 0.25
+python src/learning_curve.py experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v1 --plot-name fig1a_diwali_sweep_seeds --filter-alg-apertures DQN:5 PPO:5 PPO-RTU:5 DQN:9 PPO:9 PPO-RTU:9 --ylim "-0.1" 0.25 --subplot-by-seed
