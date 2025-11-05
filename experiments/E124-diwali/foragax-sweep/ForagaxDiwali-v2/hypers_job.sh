789522123d7f32d771940024a10773f7155f5cbc
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-11-05 10:25:07 -0700

    Create ForagaxDiwali-v2

---
+++ experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v2/hypers_job.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=aip-amw8
+#SBATCH --job-name=E124-diwali_foragax-sweep_ForagaxDiwali-v2_hypers
+#SBATCH --mem-per-cpu=8G
+#SBATCH --ntasks=1
+#SBATCH --output=../slurm-%j.out
+#SBATCH --time=01:00:00
+
+module load arrow/19
+
+cp -R .venv $SLURM_TMPDIR
+
+export MPLBACKEND=TKAgg
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export NPROC=1
+export XLA_FLAGS="--xla_cpu_multi_thread_eigen=false intra_op_parallelism_threads=1"
+export XLA_PYTHON_CLIENT_MEM_FRACTION=0.95
+export JAX_PLATFORMS=cpu
+
+$SLURM_TMPDIR/.venv/bin/python experiments/E124-diwali/foragax-sweep/ForagaxDiwali-v2/hypers.py
+
+$SLURM_TMPDIR/.venv/bin/python scripts/generate_frozen_configs.py experiments/E124-diwali/foragax/ForagaxDiwali-v2/9
+$SLURM_TMPDIR/.venv/bin/python scripts/generate_frozen_configs.py experiments/E124-diwali/foragax/ForagaxDiwali-v2/5
---
+++ experiments/E124-diwali/foragax/ForagaxDiwali-v2/slurm_search.sh
@@ -0,0 +1,2 @@
+python scripts/slurm.py --cluster clusters/vulcan-cpu.json --time 00:30:00 --entry src/continuing_main.py --force -e experiments/E124-diwali/foragax/ForagaxDiwali-v2/Baselines/Search-Nearest.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu.json --time 03:00:00 --entry src/continuing_main.py --force -e experiments/E124-diwali/foragax/ForagaxDiwali-v2/Baselines/Search-Oracle.json
