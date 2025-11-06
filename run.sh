986640a30b9e7434a898717de23c66786c6befec
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-11-05 23:13:46 -0700

    Update script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python src/rtu_ppo.py -e experiments/E124-diwali/foragax/ForagaxDiwali-v1/5/PPO.json -i 0
-python src/rtu_ppo.py -e experiments/E124-diwali/foragax/ForagaxDiwali-v1/5/PPO_frozen_1M.json -i 0
-python src/rtu_ppo.py -e experiments/E124-diwali/foragax/ForagaxDiwali-v1/5/PPO-RTU.json -i 0
-python src/rtu_ppo.py -e experiments/E124-diwali/foragax/ForagaxDiwali-v1/5/PPO-RTU_frozen_1M.json -i 0
+python src/rtu_ppo.py -e experiments/E124-diwali/foragax/ForagaxDiwali-v2/5/PPO.json -i 0
+python src/rtu_ppo.py -e experiments/E124-diwali/foragax/ForagaxDiwali-v2/5/PPO_frozen_1M.json -i 0
+python src/rtu_ppo.py -e experiments/E124-diwali/foragax/ForagaxDiwali-v2/5/PPO-RTU.json -i 0
+python src/rtu_ppo.py -e experiments/E124-diwali/foragax/ForagaxDiwali-v2/5/PPO-RTU_frozen_1M.json -i 0
