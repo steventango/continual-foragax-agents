696448b9a7cae0ac67752207166d22870f0f19d7
Steven Tang <18170455+steventango@users.noreply.github.com>
2026-02-05 14:14:36 -0700

    feat: add Search-Oracle

--- experiments/E136-big/foragax/ForagaxBig-v1/slurm_search.sh
+++ experiments/E136-big/foragax/ForagaxBig-v1/slurm_search.sh
@@ -1,3 +1,4 @@
-python scripts/slurm.py --cluster clusters/vulcan-gpu-mps.json --time 03:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v1/Baselines/Search-5.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-mps.json --time 03:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v1/Baselines/Search-9.json
-python scripts/slurm.py --cluster clusters/vulcan-gpu-mps.json --time 03:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v1/Baselines/Search-15.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-16G.json --time 03:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v1/Baselines/Search-5.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-16G.json --time 03:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v1/Baselines/Search-9.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-16G.json --time 03:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v1/Baselines/Search-15.json
+python scripts/slurm.py --cluster clusters/vulcan-cpu-16G.json --time 03:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v1/Baselines/Search-Oracle.json
