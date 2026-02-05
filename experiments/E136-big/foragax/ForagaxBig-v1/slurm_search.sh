5db71b5b22567be63ff3a4b3371f2f8f17aea508
Steven Tang <18170455+steventango@users.noreply.github.com>
2026-02-05 10:34:13 -0700

    fix: adjust configs

--- experiments/E136-big/foragax/ForagaxBig-v1/slurm_search.sh
+++ experiments/E136-big/foragax/ForagaxBig-v1/slurm_search.sh
@@ -1,5 +1,3 @@
-python scripts/slurm.py --cluster clusters/vulcan-cpu-16G.json --time 01:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v1/Baselines/Search-Nearest.json
-python scripts/slurm.py --cluster clusters/vulcan-cpu-16G.json --time 02:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v1/Baselines/Search-Oracle.json
 python scripts/slurm.py --cluster clusters/vulcan-cpu-16G.json --time 02:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v1/Baselines/Search-5.json
 python scripts/slurm.py --cluster clusters/vulcan-cpu-16G.json --time 02:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v1/Baselines/Search-9.json
 python scripts/slurm.py --cluster clusters/vulcan-cpu-16G.json --time 01:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v1/Baselines/Search-15.json
