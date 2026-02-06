84767e35015b6bfd8538025919889e1093c82a34
Steven Tang <18170455+steventango@users.noreply.github.com>
2026-02-05 22:23:57 -0700

    feat: plot search agents

--- experiments/E136-big/foragax-sweep/ForagaxBig-v2/plot.sh
+++ experiments/E136-big/foragax-sweep/ForagaxBig-v2/plot.sh
@@ -1,6 +1,3 @@
-python src/learning_curve.py experiments/E136-big/foragax-sweep/ForagaxBig-v2 --plot-name fig1a_big_sweep_mean --filter-alg-apertures Search-5 Search-7 Search-9 Search-15 Search-Oracle --metric mean_reward 
-python src/learning_curve.py experiments/E136-big/foragax-sweep/ForagaxBig-v2 --plot-name fig1a_big_sweep_cum_mean --filter-alg-apertures Search-5 Search-7 Search-9 Search-15 Search-Oracle --metric cum_mean_reward
-python src/learning_curve.py experiments/E136-big/foragax-sweep/ForagaxBig-v2 --plot-name fig1a_big_sweep_rolling --filter-alg-apertures Search-5 Search-7 Search-9 Search-15 Search-Oracle --metric rolling_reward_100000 
-for i in {1..9}; do
-python src/learning_curve.py experiments/E136-big/foragax-sweep/ForagaxBig-v2 --plot-name fig1a_big_sweep_$i --filter-alg-apertures Search-5 Search-7 Search-9 Search-15 Search-Oracle --metric ewm_reward_$i 
-done
+python src/learning_curve.py experiments/E136-big/foragax-sweep/ForagaxBig-v2 --plot-name fig1a_big_sweep_mean --filter-alg-apertures Search-5 Search-7 Search-9 Search-15 Search-31 Search-Oracle --metric mean_reward 
+python src/learning_curve.py experiments/E136-big/foragax-sweep/ForagaxBig-v2 --plot-name fig1a_big_sweep_rolling --filter-alg-apertures Search-5 Search-7 Search-9 Search-15 Search-31 Search-Oracle --metric rolling_reward_100000 
+python src/learning_curve.py experiments/E136-big/foragax-sweep/ForagaxBig-v2 --plot-name fig1a_big_sweep_9 --filter-alg-apertures Search-5 Search-7 Search-9 Search-15 Search-31 Search-Oracle --metric ewm_reward_9 
