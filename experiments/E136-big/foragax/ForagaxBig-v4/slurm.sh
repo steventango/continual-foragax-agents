4ee1c3f0f3d29a1ccaeccc77611942e8aa50c8c6
Steven Tang <18170455+steventango@users.noreply.github.com>
2026-02-23 16:09:56 -0700

    chore: DQN_LN_RT

--- experiments/E136-big/foragax/ForagaxBig-v4/slurm.sh
+++ experiments/E136-big/foragax/ForagaxBig-v4/slurm.sh
@@ -18,6 +18,8 @@ do
     python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-5.json --time 03:00:00 --runs 1 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v4/${fov}/DQN.json
     python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-5.json --time 03:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v4/${fov}/DQN_LN.json
     python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-5.json --time 03:00:00 --runs 1 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v4/${fov}/DQN_LN.json
+    python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-5.json --time 03:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v4/${fov}/DQN_LN_RT.json
+    python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-5.json --time 03:00:00 --runs 1 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v4/${fov}/DQN_LN_RT.json
     python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-5.json --time 03:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v4/${fov}/DRQN_1_1.json
     python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-5.json --time 03:00:00 --runs 1 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v4/${fov}/DRQN_1_1.json
     python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-5.json --time 03:00:00 --runs 30 --entry src/continuing_main.py --force -e experiments/E136-big/foragax/ForagaxBig-v4/${fov}/DRQN_0_2.json
