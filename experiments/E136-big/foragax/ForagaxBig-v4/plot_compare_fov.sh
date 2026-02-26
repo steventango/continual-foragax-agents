ad3cc05ab5084e9be11a4ea082fed194ee8e4bd0
Steven Tang <18170455+steventango@users.noreply.github.com>
2026-02-26 10:33:31 -0700

    feat: plot Big-v4 DRQN

--- experiments/E136-big/foragax/ForagaxBig-v4/plot_compare_fov.sh
+++ experiments/E136-big/foragax/ForagaxBig-v4/plot_compare_fov.sh
@@ -1,3 +1,4 @@
-for alg in DQN_LN PPO_LN_128 PPO-RTU_LN_128_512; do
+for alg in DQN_LN PPO_LN_128 PPO-RTU_LN_128_512 DRQN_LN_1_1; do
     python src/learning_curve.py experiments/E136-big/foragax/ForagaxBig-v4 --plot-name compare_fov/${alg}_reward --filter-alg-aperture ${alg}:5 ${alg}:9 ${alg}:15 Search-5 --metric rolling_reward_10000
+    python src/learning_bar.py experiments/E136-big/foragax/ForagaxBig-v4 --plot-name compare_fov/${alg}_reward_bar --filter-alg-apertures ${alg}:5 ${alg}:9 ${alg}:15 Search-5 --metric mean_reward
 done
--- experiments/E136-big/foragax/ForagaxBig-v4/plot_compare_layernorm.sh
+++ experiments/E136-big/foragax/ForagaxBig-v4/plot_compare_layernorm.sh
@@ -1,7 +1,14 @@
 for fov in 5 9 15; do
     for metric in reward biome_regret; do
-        python src/learning_curve.py experiments/E136-big/foragax/ForagaxBig-v4 --plot-name compare_layernorm/fig_big_dqn_fov${fov}_${metric} --filter-alg-apertures DQN:${fov} DQN_LN:${fov} Search-5 --metric rolling_${metric}_10000
-        python src/learning_curve.py experiments/E136-big/foragax/ForagaxBig-v4 --plot-name compare_layernorm/fig_big_ppo_fov${fov}_${metric} --filter-alg-apertures PPO_128:${fov} PPO_LN_128:${fov} Search-5 --metric rolling_${metric}_10000
-        python src/learning_curve.py experiments/E136-big/foragax/ForagaxBig-v4 --plot-name compare_layernorm/fig_big_rtu_fov${fov}_${metric} --filter-alg-apertures PPO-RTU_128:${fov} PPO-RTU_LN_128:${fov} Search-5 --metric rolling_${metric}_10000
+        for alg in dqn ppo rtu drqn_1_1 drqn_0_2; do
+            case $alg in
+                dqn) base="DQN"; ln="DQN_LN" ;;
+                ppo) base="PPO_128"; ln="PPO_LN_128" ;;
+                rtu) base="PPO-RTU_128"; ln="PPO-RTU_LN_128" ;;
+                drqn_1_1) base="DRQN_1_1"; ln="DRQN_LN_1_1" ;;
+                drqn_0_2) base="DRQN_0_2"; ln="DRQN_LN_0_2" ;;
+            esac
+            python src/learning_curve.py experiments/E136-big/foragax/ForagaxBig-v4 --plot-name compare_layernorm/fig_big_${alg}_fov${fov}_${metric} --filter-alg-apertures ${base}:${fov} ${ln}:${fov} Search-5 --metric rolling_${metric}_10000
+        done
     done
 done
