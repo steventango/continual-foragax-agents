d7464b660fe3862d9753bd2e3cf02527928e7bdb
Steven Tang <18170455+steventango@users.noreply.github.com>
2026-02-26 15:45:33 -0700

    feat: DQN_LN_Color baseline

--- experiments/X29-ForagaxSquareWaveTwoBiome-v10/foragax-sweep/ForagaxSquareWaveTwoBiome-v10/slurm.sh
+++ experiments/X29-ForagaxSquareWaveTwoBiome-v10/foragax-sweep/ForagaxSquareWaveTwoBiome-v10/slurm.sh
@@ -2,4 +2,5 @@ for fov in {9}
 do
     python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-256.json --time 03:00:00 --runs 5 --entry src/continuing_main.py --force -e experiments/X29-ForagaxSquareWaveTwoBiome-v10/foragax-sweep/ForagaxSquareWaveTwoBiome-v10/9/DQN_LN.json
     python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-256.json --time 03:00:00 --runs 5 --entry src/continuing_main.py --force -e experiments/X29-ForagaxSquareWaveTwoBiome-v10/foragax-sweep/ForagaxSquareWaveTwoBiome-v10/9/DQN_LN_PConv.json
+    python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-256.json --time 03:00:00 --runs 5 --entry src/continuing_main.py --force -e experiments/X29-ForagaxSquareWaveTwoBiome-v10/foragax-sweep/ForagaxSquareWaveTwoBiome-v10/9/DQN_LN_Color.json
 done
