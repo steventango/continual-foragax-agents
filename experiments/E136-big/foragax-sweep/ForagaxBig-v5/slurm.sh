3ae6c442f99f07a14ee9301e10fbb6597723127d
Steven Tang <18170455+steventango@users.noreply.github.com>
2026-03-01 01:14:13 -0700

    chore: fix script

--- experiments/E136-big/foragax-sweep/ForagaxBig-v5/slurm.sh
+++ experiments/E136-big/foragax-sweep/ForagaxBig-v5/slurm.sh
@@ -1,4 +1,4 @@
-for fov in {9}
+for fov in 9;
 do
     python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-256.json --time 01:00:00 --runs 10 --entry src/rtu_ppo.py --force -e experiments/E136-big/foragax-sweep/ForagaxBig-v5/${fov}/PPO_LN_128.json
     python scripts/slurm.py --cluster clusters/vulcan-gpu-vmap-256.json --time 01:00:00 --runs 10 --entry src/rtu_ppo.py --force -e experiments/E136-big/foragax-sweep/ForagaxBig-v5/${fov}/PPO_LN_RT_128.json
