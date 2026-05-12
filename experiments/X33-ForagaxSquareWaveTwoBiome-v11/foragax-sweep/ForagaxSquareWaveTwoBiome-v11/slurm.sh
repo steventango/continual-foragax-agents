7c71cc9979bcf8b0e379d3f1fdda9974dc619647
Steven Tang <18170455+steventango@users.noreply.github.com>
2026-05-12 12:33:32 -0600

    feat: add DQN_ReDo configs and create slurm script

---
+++ experiments/X33-ForagaxSquareWaveTwoBiome-v11/foragax-sweep/ForagaxSquareWaveTwoBiome-v11/slurm.sh
@@ -0,0 +1,7 @@
+for fov in 9;
+do
+    python scripts/slurm.py --cluster clusters/vulcan-gpu.4-vmap.json --tasks 1024 --time 03:00:00 --runs 10 --entry src/continuing_main.py --force -e experiments/X33-ForagaxSquareWaveTwoBiome-v11/foragax-sweep/ForagaxSquareWaveTwoBiome-v11/${fov}/DQN.json
+    python scripts/slurm.py --cluster clusters/vulcan-gpu.4-vmap.json --tasks 1024 --time 03:00:00 --runs 10 --entry src/continuing_main.py --force -e experiments/X33-ForagaxSquareWaveTwoBiome-v11/foragax-sweep/ForagaxSquareWaveTwoBiome-v11/${fov}/DQN_ReDo.json
+    python scripts/slurm.py --cluster clusters/vulcan-gpu.4-vmap.json --tasks 1024 --time 03:00:00 --runs 10 --entry src/continuing_main.py --force -e experiments/X33-ForagaxSquareWaveTwoBiome-v11/foragax-sweep/ForagaxSquareWaveTwoBiome-v11/${fov}/DQN_ReDo_PreActLN.json
+    python scripts/slurm.py --cluster clusters/vulcan-gpu.4-vmap.json --tasks 1024 --time 03:00:00 --runs 10 --entry src/continuing_main.py --force -e experiments/X33-ForagaxSquareWaveTwoBiome-v11/foragax-sweep/ForagaxSquareWaveTwoBiome-v11/${fov}/DQN_ReDo_PostLNScore.json
+done
